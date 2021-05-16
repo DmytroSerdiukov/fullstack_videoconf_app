ff929361512aa25bf54ae1b9bd3b389d86a0326e
Dmytro Serdiukov <60142923+DmytroSerdiukov@users.noreply.github.com>
2021-05-16 11:49:30 +0300

    set user id

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,19 +1,24 @@
 const AUTH_COMPLETED = 'AUTH_COMPLETED'
+const SET_USER_ID = 'SET_USER_ID'
 
 let initState = {
-    authIn: false
+    authIn: false,
+    userId: ''
 }
 
 const authReducer = (state = initState, action) => {
     switch(action.type) {
-        case 'AUTH_COMPLETED':
+        case AUTH_COMPLETED:
             return {...state, authIn: action.payload}
-
+        case SET_USER_ID:
+            return {...state, userId: action.payload}
         default:
             return state
     }
 }
 
-export const setAuth = (value) => ({type: 'AUTH_COMPLETED', payload: value})
+export const setAuth = (value) => ({type: AUTH_COMPLETED, payload: value})
+export const setUserId = (value) => ({type: SET_USER_ID, payload: value})
+
 
 export default authReducer
